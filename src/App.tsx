977b993d35b80f9ee90cf8baa16b92426692fe52
Lars Thorup <lars@zealake.com>
2024-01-21 10:56:46 +0100

    console.log

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+function AlbumPicker() {
+  const [albums, setAlbums] = useState<string[]>([]);
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      artist: { value: string };
+    };
+    const artist = encodeURIComponent(target.artist.value);
+    const url = `https://musicbrainz.org/ws/2/release?fmt=json&query=artist:${artist}`;
+    const response = await fetch(url);
+    const mbResult = (await response.json()) as {
+      releases: { title: string }[];
+    };
+    console.log(mbResult);
+    const { releases } = mbResult;
+    setAlbums(releases.map(({ title }) => title));
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Artist name:
+        <input name="artist" />
+      </label>
+      <button type="submit">Search</button>
+      <p>Albums:</p>
+      <ol>
+        {albums.map((album) => (
+          <li>{album}</li>
+        ))}
+      </ol>
+    </form>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +51,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <AlbumPicker />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
